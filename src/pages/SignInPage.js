1a6ccca70b1672de01c60434e52decb028a53f78
bvv94 <bvdevasconcelos@gmail.com>
2023-04-25 08:17:00 -0400

    fix: url api

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,18 +1,59 @@
 import styled from "styled-components"
-import { Link } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useContext, useState, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { UserContext } from "../Context/UserContext";
 
 export default function SignInPage() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate();
+  const {user, setUser} = useContext(UserContext)
+
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+    if (savedUser) {
+      setUser(JSON.parse(savedUser));
+      navigate("/home");
+    }
+  }, []);
+
+  function login(e) {
+
+    const body = { email, password }
+    
+    e.preventDefault();
+    console.log("Logando");
+
+    const promise = axios.post(`${apiUrl}/cadastro`, body)
+
+    //se sucesso to=/subscriptions
+    promise.then((res) => {
+        console.log(res.data)
+        // const {} = 
+        navigate("/home")
+    })
+    //se falha to=/home e exibir alert
+    promise.catch((err) =>
+        alert(err.response.data.message)
+    )
+}
+
+
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={login}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <button>Entrar</button>
+        <input onChange={e=> setEmail(e.target.value)} value={email} name="email" placeholder="E-mail" type="email" required/>
+        <input onChange={e=> setPassword(e.target.value)} value={password} name="password" placeholder="Senha" type="password" autoComplete="new-password" required />
+        <button type="submit">Entrar</button>
       </form>
 
-      <Link>
+      <Link to="/cadastro">
         Primeira vez? Cadastre-se!
       </Link>
     </SingInContainer>
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,20 +1,55 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import axios from "axios"
+import { useState } from "react"
 
 export default function SignUpPage() {
+
+  const [email, setEmail] = useState("")
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setconfirmPassword] = useState("")
+  const navigate = useNavigate();
+
+  function signUp(e) {
+
+    const body = { name, email, password }
+
+    e.preventDefault();
+    console.log("Cadastrando");
+
+    const promise = axios.post("BASE_URL", body)
+
+    //se sucesso to=/subscriptions
+    if (password === confirmPassword) {
+      promise.then((res) => {
+        console.log(res.data)
+        navigate("/subscriptions")
+      })
+    }else{
+      alert("Senha e confirme senha devem ser iguais!")
+    }
+
+    //se falha to=/home e exibir alert
+    promise.catch((err) =>
+      alert(err.response.data.message)
+    )
+  }
+
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={signUp}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
+        <input onChange={e => setName(e.target.value)} value={name} placeholder="Nome" type="text" required />
+        <input onChange={e => setEmail(e.target.value)} value={email} placeholder="E-mail" type="email" required />
+        <input onChange={e => setPassword(e.target.value)} value={password} placeholder="Senha" type="password" autocomplete="new-password" required />
+        <input onChange={e => setconfirmPassword(e.target.value)} value={confirmPassword} placeholder="Confirme a senha" type="password" autocomplete="new-password" required />
         <button>Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to="/">
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
